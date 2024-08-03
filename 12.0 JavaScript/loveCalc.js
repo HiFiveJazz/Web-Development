7425ba8ffc50103a5bb18477872f83f75135ff11
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-08-03 00:04:15 -0700

    LoveCalc finish

--- 12.0 JavaScript/loveCalc.js
+++ 12.0 JavaScript/loveCalc.js
@@ -1,1 +1,8 @@
-test
+prompt("Enter in your name?")
+prompt("Enter in the name of your crush?")
+var n = Math.random();
+n = (n * 100) + 1;
+n = Math.floor(n);
+alert("Your love score is: " + n + "%")
+
+
