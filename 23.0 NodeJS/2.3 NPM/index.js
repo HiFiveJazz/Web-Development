2d67e657d235cd67632c4d08e7052c7bf096b46c
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-12-10 16:55:51 -0800

    superhero stats

---
+++ 23.0 NodeJS/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// var generateName = require("sillyname");
+// var test = generateName();
+//
+// console.log(`My name is ${test}.`);
+
+import superheroes from 'superheroes';
+
+console.log(`${superheroes[0]}`);
