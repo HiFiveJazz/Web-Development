3c0cdada9b8508656f0bf22794655f6ba9b890a4
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-12-11 23:55:45 -0800

    updating packages

--- 23.0 NodeJS/2.4 QR Code Project/index.js
+++ 23.0 NodeJS/2.4 QR Code Project/index.js
@@ -4,19 +4,19 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 import inquirer from 'inquirer';
+// var qr = require('qr-image');
+  const questions = [
+  {
+    // type: 'input',
+    name: 'link',
+    message: 'Insert the link of the QR code you want generated!'
+  }
+]
 
-inquirer
-  .prompt([console.log('Prompt')
-    
-  ])
-  .then((answers) => {
-    console.log(answers);
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-
-    } else {
-
-    }
-  });
-inquirer.prompt
+inquirer.prompt(questions).then((answers) => {
+  const url = answers.URL;
+  console.log(url);
+  // var qr_png = qr.image('I love QR!', { type: 'png'});
+  // qr_png.pipe(require('fs').createWriteStream('qr_code.png'));
+  // var qr_string =  qr.imageSync('I love QR!', { type: 'png'});
+});
