a3e89f1bb0055da08f2f49ac31e5dc825605db27
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-12-18 16:07:59 -0800

    completed NODEJS

--- 23.0 NodeJS/2.4 QR Code Project/index.js
+++ 23.0 NodeJS/2.4 QR Code Project/index.js
@@ -4,19 +4,42 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
 // var qr = require('qr-image');
-  const questions = [
+
+inquirer
+  .prompt([
   {
     // type: 'input',
-    name: 'link',
-    message: 'Insert the link of the QR code you want generated!'
+    message: 'Insert the link of the QR code you want generated!',
+    name: 'link'
   }
-]
+])
+  .then((answers) => {
+    const url = answers.link;
+    // PNG
+    // var qr_svg = qr.image(url);
+    // qr_svg.pipe(fs.createWriteStream('qr_img.png', {size: 10}));
+    // SVG
+    var qr_svg = qr.image(url, { type: 'svg', size: '10' });
+    qr_svg.pipe(fs.createWriteStream('qr_image.svg'));
+    qr_svg.svgObject(url);
 
-inquirer.prompt(questions).then((answers) => {
-  const url = answers.URL;
-  console.log(url);
-  // var qr_png = qr.image('I love QR!', { type: 'png'});
-  // qr_png.pipe(require('fs').createWriteStream('qr_code.png'));
-  // var qr_string =  qr.imageSync('I love QR!', { type: 'png'});
-});
+    fs.writeFileSync('./URLs.txt',url);
+  })
+  .catch((error) => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in current environment
+    } else {
+      // Something else went wrong
+    }
+  })
+
+// inquirer.prompt(questions).then((answers) => {
+//   const url = answers.URL;
+//   console.log(url);
+//   // var qr_png = qr.image('I love QR!', { type: 'png'});
+//   // qr_png.pipe(require('fs').createWriteStream('qr_code.png'));
+//   // var qr_string =  qr.imageSync('I love QR!', { type: 'png'});
+// })
